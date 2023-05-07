f2f6e1822eb046aa980c52b6c57e25a24fb3e56d
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-05-07 03:44:45 -0500

    test: refactor and add "Crear borrador" test case

--- page-objects/post-page.ts
+++ page-objects/post-page.ts
@@ -8,6 +8,43 @@ export class PostPage {
     this.page = page;
   }
 
+  async createPost(title: string, content: string): Promise<string> {
+    await this.navigateToPostEditor();
+    await this.fillPostTitle(title);
+    await this.fillPostContent(content);
+    await this.publishPost();
+    const postId = await this.getPostIdFromUrl();
+    return postId;
+  }
+
+  async updatePost() {
+    await this.page.getByRole("button", { name: "Update" }).click();
+    await this.page.getByRole("button", { name: "Update", exact: true }).click();
+    await this.page.waitForTimeout(1000);
+  }
+
+  async deletePost() {
+    await this.page.getByRole("button", { name: "Settings" }).click();
+    await this.page.getByRole("button", { name: "Delete post" }).click();
+    await this.page.click(".gh-btn.gh-btn-red.gh-btn-icon");
+  }
+
+  async updatePostById(postId: string, updatedPost: { title: string; content: string }): Promise<void> {
+    await this.navigateToPostById(postId);
+    await this.fillPostTitle(updatedPost.title);
+    await this.fillPostContent(updatedPost.content);
+    await this.updatePost();
+  }
+
+  async deletePostById(postId: string): Promise<void> {
+    await this.navigateToPostById(postId);
+    await this.deletePost();
+  }
+
+  async navigateToPostById(postId: string) {
+    await this.page.goto(`${config.baseUrl}/ghost/#/editor/post/${postId}`);
+  }
+
   async navigateToPostList() {
     await this.page.goto(`${config.baseUrl}/ghost/#/posts/`);
   }
@@ -16,22 +53,12 @@ export class PostPage {
     await this.page.goto(`${config.baseUrl}/ghost/#/editor/post/`);
   }
 
-  async publishUpdatedPost() {
-    await this.page.getByRole("button", { name: "Update" }).click();
-    await this.page.getByRole("button", { name: "Update", exact: true }).click();
-    await this.page.waitForTimeout(1000);
-  }
-
-  async expectPostUpdatedSuccessfully(updatedTitle: string) {
-    await this.page.waitForSelector(`.gh-content-entry-title:has-text("${updatedTitle}")`);
-    const postTitleElement = await this.page.$(`.gh-content-entry-title:has-text("${updatedTitle}")`);
-    expect(postTitleElement).toBeTruthy();
-  }
-
-  async getPostListLength(): Promise<number> {
-    await this.page.waitForSelector(".posts-list");
-    const listElements = await this.page.$$(".gh-posts-list-item");
-    return listElements.length;
+  async getPostIdFromUrl(): Promise<string> {
+    const currentUrl = await this.page.url();
+    const url = new URL(currentUrl);
+    const fragments = url.hash.split("/");
+    const postId = fragments[fragments.length - 1];
+    return postId;
   }
 
   async fillPostTitle(title: string) {
@@ -56,14 +83,21 @@ export class PostPage {
     expect(notificationElement).toBeTruthy();
   }
 
-  async selectPostByIndex(index: number) {
-    await this.page.waitForSelector(".posts-list");
-    const postElements = await this.page.$$(".gh-posts-list-item");
-    await postElements[index].click();
+  async getErrorMessageText(): Promise<string | null> {
+    return await this.page.textContent(".midlightgrey.error-code-size");
   }
 
-  async filterPublishedPosts() {
-    await this.page.getByRole("button", { name: "All posts" }).click();
-    await this.page.getByText("Published posts").click();
+  async getPostTitle(): Promise<string | null> {
+    const titleElement = await this.page.locator(".gh-editor-title");
+    return await titleElement.evaluate((el) => (el as HTMLTextAreaElement).value);
+  }
+
+  async getPostContent(): Promise<string | null> {
+    return await this.page.locator(".koenig-editor__editor").textContent();
+  }
+
+  async expectPostStatus(status: string) {
+    const draftElement = this.page.locator("header").getByText(status);
+    await expect(draftElement).toBeVisible();
   }
 }
--- tests/post.spec.ts
+++ tests/post.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { LoginPage } from "../page-objects/login-page";
 import { PostPage } from "../page-objects/post-page";
 import { config } from "../page-objects/config";
@@ -6,35 +6,52 @@ import { config } from "../page-objects/config";
 test.describe("Posts", () => {
   let loginPage: LoginPage;
   let postPage: PostPage;
+  let postId: string;
+  const title = "New Title";
+  const content = "New Content";
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
     postPage = new PostPage(page);
     await loginPage.navigate();
     await loginPage.login(config.email, config.password);
+    postId = await postPage.createPost(title, content);
   });
 
   test("Crear post", async () => {
-    const title = "My Updated Post Title";
-    const content = "My Updated Post Content";
-
-    await postPage.navigateToPostEditor();
-    await postPage.fillPostTitle(title);
-    await postPage.fillPostContent(content);
-    await postPage.publishPost();
+    await postPage.createPost(title, content);
     await postPage.expectNotificationShown("Published");
+    await postPage.expectPostStatus("Published");
   });
 
   test("Actualizar post", async () => {
-    const updatedTitle = "My Updated Post Title";
-    const updatedContent = "My Updated Post Content";
-
-    await postPage.navigateToPostList();
-    await postPage.filterPublishedPosts();
-    await postPage.selectPostByIndex(0);
-    await postPage.fillPostTitle(updatedTitle);
-    await postPage.fillPostContent(updatedContent);
-    await postPage.publishUpdatedPost();
+    const updatedPost = { title: "Updated Title", content: "Updated Content" };
+    await postPage.updatePostById(postId, updatedPost);
     await postPage.expectNotificationShown("Updated");
+  });
+
+  test("Eliminar post", async () => {
+    await postPage.deletePostById(postId);
+    await postPage.navigateToPostById(postId);
+
+    const errorCode = await postPage.getErrorMessageText();
+    expect(errorCode).toBe("404");
+  });
+
+  test("Leer post", async () => {
+    await postPage.navigateToPostById(postId);
+    const postTitle = await postPage.getPostTitle();
+    const postContent = await postPage.getPostContent();
+    expect(postTitle).toBe(title);
+    expect(postContent).toBe(content);
+  });
+
+  test("Crear borrador", async () => {
+    const postTitle = "New Title";
+    const postContent = "New Content";
+    await postPage.navigateToPostEditor();
+    await postPage.fillPostTitle(postTitle);
+    await postPage.fillPostContent(postContent);
+    await postPage.expectPostStatus("Draft");
   });
 });
